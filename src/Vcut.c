4126b1b66246c9b7233e487ca38f7ae6bbf3a4cd
Nicholas Henderson <nchender@sph-190157-bio.ddns.sph.umich.edu>
2019-09-26 12:11:21 -0400

    Just changed so that the comparison between local and marginal V-curves is more direct

--- src/Vcut.c
+++ src/Vcut.c
@@ -16,13 +16,12 @@ SEXP Vcut(SEXP Vmat, SEXP lamfun, SEXP nunits, SEXP ngrid, SEXP Agrid) {
   
   for (ii=0; ii < nrow; ii++) {
      for(jj=0; jj < ncol; jj++)  {
-         tst = REAL(Vmat)[ii + nrow*jj] - REAL(lamfun)[jj];
+         // tst = REAL(Vmat)[ii + nrow*jj] - REAL(lamfun)[jj];
          // maybe change the tst>= 0 condition or
          // compare(Vmat) with (lamfun) directly
          // Interpolation:
          
-         if(tst > 0) {
-            //tstp1 = REAL(Vmat)[ii + nrow*jj - 1] - REAL(lamfun)[jj - 1];
+         if(REAL(Vmat)[ii + nrow*jj] > REAL(lamfun)[jj]) {
             // check if "jj=0"
             if(jj > 0) {
                 g0 = REAL(Vmat)[ii + nrow*jj - 1] - REAL(lamfun)[jj - 1]; 
